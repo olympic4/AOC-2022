7301ad099a4eac38d25b18f5d8034b4a37cf0b7f
j0nn <68810303+olympic4@users.noreply.github.com>
2022-12-01 22:20:26 +0000

    Improved readability of Day1.py

--- Day1.py
+++ Day1.py
@@ -1,6 +1,11 @@
 input = open("input.txt","r").read()
 
-elvesTotals = sorted(map(sum,[map(int,elf.splitlines()) for elf in input.split("\n\n")]),reverse=True)
+elvesTotals = sorted( #sorted list of total calories carried by each elf
+    map(sum,[
+        map(int,elf.splitlines())
+        for elf in input.split("\n\n")
+    ]),reverse=True
+)
 
 print("PART 1:",elvesTotals[0])
 print("PART 2:",sum(elvesTotals[0:3]))
