5f55e8dee2f2096780ee31edfd9aa531d07cd6bf
j0nn <68810303+olympic4@users.noreply.github.com>
2022-12-03 20:03:43 +0000

    Solved

--- Day3.py
+++ Day3.py
@@ -1,12 +1,25 @@
 input = open("input.txt","r").read()
+rucksacks = input.splitlines()
 
 alphabet_priority_order = [chr(v) for v in range(ord("a"), ord("z")+1)]+([chr(v) for v in range(ord("A"), ord("Z")+1)])
 
-def search(rucksack):
+def search1(rucksack):
     firstCompartment = rucksack[:len(rucksack)//2]
     secondCompartment = rucksack[len(rucksack)//2:]
     for item in firstCompartment:
         if item in secondCompartment:
             return alphabet_priority_order.index(item)+1
 
-print("PART 1:",sum(search(rucksack) for rucksack in input.splitlines()))
+def search2(group):
+    for item in group[0]:
+        if item in group[1] and item in group[2]:
+            return alphabet_priority_order.index(item)+1
+
+groups = [
+    rucksacks[rucksacks.index(rucksack)-2:rucksacks.index(rucksack)+1] 
+    for rucksack in rucksacks
+    if (rucksacks.index(rucksack)+1)%3 == 0
+]
+
+print("PART 1:",sum(search1(rucksack) for rucksack in rucksacks))
+print("PART 2:",sum(search2(group) for group in groups))
